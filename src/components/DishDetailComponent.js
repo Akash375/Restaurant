53e5f0c3656a29d25206565a1c120fb4e424a87d
Akash375 <50778059+Akash375@users.noreply.github.com>
2020-07-30 15:43:50 +0530

    Final

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal,
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal,
      ModalHeader, ModalBody, Label, Row, Col  } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
